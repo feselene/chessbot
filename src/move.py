eeae0f8eb1f7f4f4722c50d120b9118820e643a0
feselene <vaporflyb@gmail.com>
2024-10-31 08:59:20 -0400

    Crop the screenshot and save to cropped_screenshot.png before predicting

--- src/move.py
+++ src/move.py
@@ -1,4 +1,5 @@
 import pyautogui
+import cv2
 from pynput.mouse import Listener
 import mss
 import time
@@ -23,11 +24,30 @@ result = CLIENT.infer("screenshot.png", model_id="chessboard-segmentation/1")
 
 print(result)
 
-CLIENT = InferenceHTTPClient(
+image = cv2.imread("screenshot.png")
+if (result['predictions']):
+    x = int(result['predictions'][0]['x'])
+    y = int(result['predictions'][0]['y'])
+    width = int(result['predictions'][0]['width'])
+    height = int(result['predictions'][0]['height'])
+
+    # Calculate the top-left and bottom-right corners
+    top_left = (x - width // 2, y - height // 2)
+    bottom_right = (x + width // 2, y + height // 2)
+
+    # Crop the image using the calculated coordinates
+    cropped_image = image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+    # Save the cropped image
+    cv2.imwrite("cropped_screenshot.png", cropped_image)
+
+    print(result)
+    CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
     api_key="23rLdZSuKttIzDN4MKCd"
 )
 
-result = CLIENT.infer("screenshot.png", model_id="chessbot-v2/1")
-
-print(result)
+    result = CLIENT.infer("cropped_screenshot.png", model_id="2dcpd/2")
+    print(result)
+else:
+    print("No prediction available. ")
